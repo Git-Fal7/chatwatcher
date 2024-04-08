57bd2e53026899fa499a02ea3dad473f98eb4b5c
* <61941069+Git-Fal7@users.noreply.github.com>
2024-04-08 03:16:40 +0300

    Forgot to init config

--- internal/plugin/plugin.go
+++ internal/plugin/plugin.go
@@ -3,12 +3,14 @@ package plugin
 import (
 	"context"
 
+	"github.com/git-fal7/chatwatcher/internal/config"
 	"github.com/git-fal7/chatwatcher/internal/event"
 	"github.com/git-fal7/chatwatcher/internal/profanityfilter"
 	"go.minekube.com/gate/pkg/edition/java/proxy"
 )
 
 func InitPlugin(ctx context.Context, proxy *proxy.Proxy) error {
+	config.InitConfig()
 	profanityfilter.InitProfanityFilter()
 	event.Init(proxy)
 	return nil
